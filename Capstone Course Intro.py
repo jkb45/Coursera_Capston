d1d6a0ae491cae6b367b2989fb7e55191a9a35a2
jkb45 <48390321+jkb45@users.noreply.github.com>
2020-01-05 21:11:37 -0600

    Add files via upload

---
+++ Capstone Course Intro.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This Jupyter notebook will be used for the IBM Applied Data Science Capstone project.
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
